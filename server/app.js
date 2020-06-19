7b7e0a1a82d18755be005eca1f5e6183fb948e65
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2020-06-18 22:36:47 -0300

    Modelos Criados - Controllers a caminho!

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// Importamos o código do módulo database e usamos a função connection para conectar ao banco de dados.
+const database = require('./src/config/database');
+database('mongodb://localhost:27017/BlogGMRelacional');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
